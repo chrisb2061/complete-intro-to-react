68b235a4da320fb59aae1ad2c8fe5eddc7378f3f
Christian Barnard <cbarnard@RSG-4283-Christian-Barnard.local>
2016-11-06 21:36:09 -0500

    extract MyTitle and use require

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,26 +1,18 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
+
+
 var div = React.DOM.div
-var h1 = React.DOM.h1
 
-var myTitle = React.createClass({
-
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-
-})
-
-var myTitleFactory = React.createFactory(myTitle);
+var myTitleFactory = React.createFactory(MyTitle)
 var ce = React.createElement
 
 var myFirstComponent = (
-  div(null,
-    myTitleFactory({title: "Props are Great", color: "rebeccapurple"}),
-    ce(myTitle, {title: "Pops are the best", color: "mediumaquamarine"}),
-    React.createElement(myTitle, {title: "use props everyehere", color: "papayawhip"})
-  )
+    div(null,
+        myTitleFactory({title: 'Props are Great', color: 'rebeccapurple'}),
+        ce(MyTitle, {title: 'Pops are the best', color: 'mediumaquamarine'}),
+        React.createElement(MyTitle, {title: 'use props everywhere', color: 'papayawhip'})
+    )
 )
-ReactDOM.render(myFirstComponent, document.getElementById("app"))
+ReactDOM.render(myFirstComponent, document.getElementById('app'))
