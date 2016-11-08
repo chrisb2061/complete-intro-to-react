690acac47c96efa045e2aa50b3867964e6505b8a
Christian Barnard <cbarnard@RSG-4283-Christian-Barnard.local>
2016-11-07 22:19:45 -0500

    adding eslint to webpack

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,28 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 
-const App =  () => {
-  return (
-    <div className='app-container'>
-      <div className="home-info">
-        <h1 className='title'>svideo</h1>
-        <input type='text' className='search' placeholder='Search' />
-        <button className='browse-all'>or Browse All</button>
-      </div>
+/*
+*     these two are the same
+*     () => ()
+*
+*     whatever is in the second parenthesis will be returned automatically
+*
+*     function () {
+*
+*       return
+*
+*     }
+*
+* */
+
+const App = () => (
+  <div className='app-container'>
+    <div className="home-info">
+      <h1 className='title'>svidec</h1>
+      <input type='text' className='search' placeholder='Search' />
+      <button className='browse-all'>or Browse All</button>
     </div>
-  )
-}
+  </div>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'))
