53ff3c24a34b901e46e92500a4e7d27509f93397
Christian Barnard <cbarnard@RSG-4283-Christian-Barnard.local>
2016-11-12 14:16:46 -0500

    adding layout

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,7 +3,10 @@ const ReactDOM = require('react-dom')
 
 // Routing
 const ReactRouter = require('react-router')
-const { Router, Route, hashHistory } = ReactRouter
+const { Router, Route, IndexRoute, hashHistory } = ReactRouter
+
+// Layout
+const Layout = require('./layout')
 
 // Page components
 const Landing = require('./Landing')
@@ -11,10 +14,11 @@ const Search = require('./Search')
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
-
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,12 +2,10 @@ const React = require('react')
 const { Link } = require('react-router')
 
 const Landing = () => (
-  <div className='app-container'>
-    <div className="home-info">
-      <h1 className='title'>svidec</h1>
-      <input type='text' className='search' placeholder='Search' />
-      <Link to='/search' className='browse-all'>or Browse All</Link>
-    </div>
+  <div className="home-info">
+    <h1 className='title'>svidec</h1>
+    <input type='text' className='search' placeholder='Search' />
+    <Link to='/search' className='browse-all'>or Browse All</Link>
   </div>
 )
 
