3618e35a094d1dd6128e7538c2592a389d245f18
Christian Barnard <cbarnard@RSG-4283-Christian-Barnard.local>
2016-11-12 17:31:21 -0500

    filter movies with Search

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,7 +5,7 @@ const ShowCard = require('./ShowCard')
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'This is my search term'
+      searchTerm: ''
     }
   },
   handleSearchTermEvent (event) {
@@ -20,8 +20,18 @@ const Search = React.createClass({
           <input value={this.state.searchTerm} type='text' placeholder='Search' className='search-input' onChange={this.handleSearchTermEvent} />
         </header>
         <div className='shows'>
-          {data.shows.map((show) => (
-            <ShowCard {...show} key={show.imdbID} />
+          {data.shows
+            //  checks if search term matches anything in show.title or show.description
+            .filter((show) => {
+              //  same as show.title + show.descrition (es6 concatination)
+              let searchString = `${show.title} ${show.description}`
+                .toUpperCase()
+                .indexOf(this.state.searchTerm.toUpperCase())
+
+              return searchString >= 0
+            })
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
           ))}
         </div>
       </div>
